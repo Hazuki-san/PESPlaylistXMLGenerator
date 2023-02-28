744f4302556f6ddf7981e2fa7e2885d1ec8586f1
Hazuki Onohara <aoba@outlook.co.th>
2023-03-01 01:31:09 +0700

    initial commit
    thanks ChatGPT

---
+++ PESPlaylistXMLGenerator/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿using System.Windows.Forms;
+
+namespace PESPlaylistXMLGenerator
+{
+    partial class PESPlaylistXMLData : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.xmlData = new System.Windows.Forms.DataGridView();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Artist = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.pesVersions = new System.Windows.Forms.ComboBox();
+            this.someRandoText = new System.Windows.Forms.Label();
+            this.deleteRow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.xmlData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xmlData
+            // 
+            this.xmlData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.xmlData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Artist});
+            this.xmlData.Location = new System.Drawing.Point(12, 12);
+            this.xmlData.Name = "xmlData";
+            this.xmlData.Size = new System.Drawing.Size(560, 400);
+            this.xmlData.TabIndex = 0;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            this.Title.Width = 275;
+            // 
+            // Artist
+            // 
+            this.Artist.HeaderText = "Artist";
+            this.Artist.MaxInputLength = 64;
+            this.Artist.Name = "Artist";
+            this.Artist.Width = 220;
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(12, 429);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 1;
+            this.openBtn.Text = "Open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(92, 429);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // pesVersions
+            // 
+            this.pesVersions.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.pesVersions.FormattingEnabled = true;
+            this.pesVersions.Items.AddRange(new object[] {
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021"});
+            this.pesVersions.Location = new System.Drawing.Point(496, 430);
+            this.pesVersions.Name = "pesVersions";
+            this.pesVersions.Size = new System.Drawing.Size(75, 21);
+            this.pesVersions.TabIndex = 3;
+            this.pesVersions.SelectedIndex = 4;
+            // 
+            // someRandoText
+            // 
+            this.someRandoText.AutoSize = true;
+            this.someRandoText.Location = new System.Drawing.Point(258, 427);
+            this.someRandoText.Name = "someRandoText";
+            this.someRandoText.Size = new System.Drawing.Size(235, 26);
+            this.someRandoText.TabIndex = 4;
+            this.someRandoText.Text = "Select your PES version, if there\'s nothing you\'re\r\nlooking for then just put you" +
+    "r PES version in box";
+            this.someRandoText.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // deleteRow
+            // 
+            this.deleteRow.Location = new System.Drawing.Point(172, 429);
+            this.deleteRow.Name = "deleteRow";
+            this.deleteRow.Size = new System.Drawing.Size(75, 23);
+            this.deleteRow.TabIndex = 5;
+            this.deleteRow.Text = "Delete";
+            this.deleteRow.UseVisualStyleBackColor = true;
+            this.deleteRow.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // PESPlaylistXMLData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.deleteRow);
+            this.Controls.Add(this.someRandoText);
+            this.Controls.Add(this.pesVersions);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.xmlData);
+            this.MaximizeBox = false;
+            this.Name = "PESPlaylistXMLData";
+            this.Text = "PES Playlist.xml Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.xmlData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private DataGridView xmlData;
+        private Button openBtn;
+        private Button saveBtn;
+        private DataGridViewTextBoxColumn Title;
+        private DataGridViewTextBoxColumn Artist;
+        private ComboBox pesVersions;
+        private Label someRandoText;
+        private Button deleteRow;
+    }
+}
---
+++ PESPlaylistXMLGenerator/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml;
+using System.Globalization;
+
+namespace PESPlaylistXMLGenerator
+{
+    public partial class PESPlaylistXMLData : Form
+    {
+        public PESPlaylistXMLData()
+        {
+            InitializeComponent();
+        }
+
+        private string filePath;
+        private DataTable dataTable = new DataTable();
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "XML Files|*.xml";
+            openFileDialog1.Title = "Select a Playlist File";
+            openFileDialog1.FileName = "Playlist.xml";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                dataTable.Clear(); // Clear the table before adding new data
+                xmlData.Rows.Clear(); // Clear the rows
+
+                try
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(filePath);
+                    XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/Playlist/CueList/Label");
+
+                    foreach (XmlNode node in nodeList)
+                    {
+                        string title = node.Attributes["Title"].Value;
+                        string artist = node.Attributes["Artist"].Value;
+
+                        if (!dataTable.Columns.Contains("Title")) // check if Title column exists
+                        {
+                            dataTable.Columns.Add("Title", typeof(string)); // add Title column if it doesn't exist
+                        }
+
+                        if (!dataTable.Columns.Contains("Artist")) // check if Artist column exists
+                        {
+                            dataTable.Columns.Add("Artist", typeof(string)); // add Artist column if it doesn't exist
+                        }
+
+                        dataTable.Rows.Add(title, artist);
+                    }
+
+                    // Copy the rows from the dataTable to the xmlData DataGridView
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        xmlData.Rows.Add(row.ItemArray);
+                    }
+
+                    MessageBox.Show("Data loaded successfully.", "PES Playlist.xml Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private DataTable CreateResultDataTable()
+        {
+            DataTable result = new DataTable();
+            foreach (DataGridViewColumn col in xmlData.Columns)
+            {
+                result.Columns.Add(col.HeaderText);
+            }
+            for (int i = 0; i < xmlData.Rows.Count - 1; i++)
+            {
+                DataGridViewRow row = xmlData.Rows[i];
+                DataRow dataRow = result.NewRow();
+                for (int j = 0; j < xmlData.Columns.Count; j++)
+                {
+                    dataRow[j] = row.Cells[j].Value;
+                }
+                result.Rows.Add(dataRow);
+            }
+            return result;
+        }
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    MessageBox.Show("Please open a file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                DataTable dataTable = CreateResultDataTable();
+
+                if (dataTable == null || dataTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("There is no data to save.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                XmlDocument xmlDoc = new XmlDocument();
+                XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", null, null);
+                xmlDeclaration.Standalone = "yes";
+                xmlDoc.AppendChild(xmlDeclaration);
+
+                XmlElement root = xmlDoc.CreateElement("Playlist");
+                root.SetAttribute("converter_version", "20230001"); //
+                root.SetAttribute("wepes", GetYearFromComboBox());
+                root.SetAttribute("target", "CS");
+                root.SetAttribute("version", "0");
+                root.SetAttribute("create", DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss", CultureInfo.InvariantCulture));
+                xmlDoc.AppendChild(root);
+
+                XmlElement cueList = xmlDoc.CreateElement("CueList");
+                cueList.SetAttribute("Num", dataTable.Rows.Count.ToString());
+                root.AppendChild(cueList);
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    // Create a CueName attribute based on the row index
+                    string cueName = string.Format("BGM0_LIC_MENU_{0:00}", dataTable.Rows.IndexOf(row) + 1);
+                    int songId = dataTable.Rows.IndexOf(row) + 19000;
+
+                    XmlElement label = xmlDoc.CreateElement("Label");
+                    label.SetAttribute("CueName", cueName);
+                    label.SetAttribute("Id", songId.ToString());
+                    label.SetAttribute("Title", row["Title"].ToString());
+                    label.SetAttribute("Artist", row["Artist"].ToString());
+                    label.SetAttribute("Offset", "0");
+                    label.SetAttribute("RemainTimeForFadeoutStart", "0");
+                    label.SetAttribute("FadeoutTime", "0");
+                    label.SetAttribute("FadeoutWaitTime", "0");
+
+                    // Add the Label element to the CueList element
+                    cueList.AppendChild(label);
+                }
+
+
+                XmlElement defaultPlayList = xmlDoc.CreateElement("DefaultPlayList");
+                defaultPlayList.SetAttribute("Num", "2");
+                root.AppendChild(defaultPlayList);
+
+                XmlElement menuList = xmlDoc.CreateElement("List");
+                menuList.SetAttribute("ListName", "Menu");
+                menuList.SetAttribute("NumContents", dataTable.Rows.Count.ToString());
+                defaultPlayList.AppendChild(menuList);
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    int songId = dataTable.Rows.IndexOf(row) + 19000;
+                    XmlElement label = xmlDoc.CreateElement("Label");
+                    label.SetAttribute("Id", songId.ToString());
+                    menuList.AppendChild(label);
+                }
+
+                XmlElement highlightList = xmlDoc.CreateElement("List");
+                highlightList.SetAttribute("ListName", "Highlight");
+                highlightList.SetAttribute("OffsetStart", "1");
+                highlightList.SetAttribute("NumContents", dataTable.Rows.Count.ToString());
+                defaultPlayList.AppendChild(highlightList);
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    int songId = dataTable.Rows.IndexOf(row) + 19000;
+                    XmlElement label = xmlDoc.CreateElement("Label");
+                    label.SetAttribute("Id", songId.ToString());
+                    highlightList.AppendChild(label);
+                }
+
+                XmlElement regionTopBgm = xmlDoc.CreateElement("RegionTopBgm");
+                regionTopBgm.SetAttribute("Num", "1");
+                root.AppendChild(regionTopBgm);
+
+                XmlElement regionTopBgmLabel = xmlDoc.CreateElement("Label");
+                regionTopBgmLabel.SetAttribute("Region", "def");
+                regionTopBgmLabel.SetAttribute("Id", "-1");
+                regionTopBgm.AppendChild(regionTopBgmLabel);
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.FileName = "Playlist.xml";
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = saveFileDialog1.FileName;
+
+                    // Create XmlWriterSettings with indent set to true
+                    XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.Indent = true;
+                    settings.IndentChars = "\t";
+
+                    // Use XmlWriter to write the XML to the file with the specified settings
+                    using (XmlWriter writer = XmlWriter.Create(filePath, settings))
+                    {
+                        xmlDoc.Save(writer);
+                    }
+
+                    MessageBox.Show("Data saved successfully.", "PES Playlist.xml Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (xmlData.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select at least one row to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Are you sure you want to delete the selected row(s)?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                // Loop through the selected rows and delete them
+                foreach (DataGridViewRow row in xmlData.SelectedRows)
+                {
+                    // Skip the new row
+                    if (row.IsNewRow)
+                    {
+                        continue;
+                    }
+                    xmlData.Rows.RemoveAt(row.Index);
+                }
+            }
+        }
+
+        private string GetYearFromComboBox()
+        {
+            string selectedValue = pesVersions.SelectedItem.ToString();
+            string year = selectedValue.Substring(selectedValue.Length - 2);
+            return year;
+        }
+
+
+    }
+}
